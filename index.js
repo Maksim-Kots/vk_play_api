e6f6f99a7969c979ed171643d4864840ce5e7e03
Maksim-Kots <kots-maks@mail.ru>
2023-03-09 08:56:58 +0300

    3 commit

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database('./db/steam.db')
+
 var app = express();
 
 
@@ -8,4 +11,39 @@ app.listen(8000, () => {
 
 app.get('/', (req, res) =>{
     res.json('Молодец!')
+})
+
+app.get('/games', (req, res) => {
+    db.all(`SELECT * FROM GAMES`, (err, rows) => {
+        res.json(rows)
+    })
+
+})
+
+app.get('/developer', (req, res) => {
+    db.all(`SELECT * FROM DEVELOPER`, (err, rows) => {
+        res.json(rows)
+    })
+
+})
+
+app.get('/category', (req, res) => {
+    db.all(`SELECT * FROM CATEGORY`, (err, rows) => {
+        res.json(rows)
+    })
+
+})
+
+app.get('/genre', (req, res) => {
+    db.all(`SELECT * FROM GENRE`, (err, rows) => {
+        res.json(rows)
+    })
+
+})
+
+app.get('/type', (req, res) => {
+    db.all(`SELECT * FROM TYPE`, (err, rows) => {
+        res.json(rows)
+    })
+
 })
